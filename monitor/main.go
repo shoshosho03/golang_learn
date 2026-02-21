41cdbf3fee466fb352bd4d7da2cedbe91ed7528f
shoshosho03 <lvphralexi3@gmail.com>
2026-02-21 23:09:04 +0900

    ループ処理を追加。ついでに監視間隔の指定方法を修正

--- monitor/main.go
+++ monitor/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 
@@ -9,17 +10,21 @@ import (
 )
 
 func main() {
+	// 監視間隔をコマンドライン引数から取得
+	interval := flag.Duration("interval", 1*time.Second, "監視間隔を秒単位で指定")
+	flag.Parse()
+
 	v, _ := mem.VirtualMemory()
 	c, _ := cpu.Percent(time.Second, false)
-	// almost every return value is a struct
-	//fmt.Printf("TotalMem: %d, Free:%d, UsedPercent:%.2f%%\n", v.Total, v.Free, v.UsedPercent)
-	fmt.Printf("UsedPercent:%.2f%%\n", v.UsedPercent)
-	fmt.Printf("CPU Percent: %.2f%%\n", c[0])
 
-	// convert to JSON. String() is also implemented
-	//fmt.Println(v)
-	//fmt.Println(c)
+	for {
+		// システムリソースの使用状況を表示
+		fmt.Printf("UsedPercent:%.2f%%\n", v.UsedPercent)
+		fmt.Printf("CPU Percent: %.2f%%\n", c[0])
 
-	fmt.Println("Monitoring system resources for 3 seconds...")
-	time.Sleep(3 * time.Second)
+		// 指定された間隔だけ待機
+		fmt.Println("Monitoring system resources for", *interval)
+		time.Sleep(*interval)
+	}
+
 }
